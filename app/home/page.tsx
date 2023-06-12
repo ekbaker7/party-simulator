affcddb6355d8e039e34310b109b58befbac3859
Emmett Baker <ekbaker@proton.me>
2023-06-12 17:40:36 -0400

    fix apos escaping

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,8 +3,8 @@ function HomePage(props: any) {
     <div className="bg-slate-600 h-[calc(100vh-50px)] overflow-hidden">
       <div className="text-white p-10">
         <p>
-          This is the home page after logging in. It's kinda empty right now,
-          but believe me, it's gonna have some stuff. Eventually.
+          This is the home page after logging in. It&apos;s kinda empty right now,
+          but believe me, it&apos;s gonna have some stuff. Eventually.
         </p>
         <p>
           Like, imagine a character right here.
@@ -19,7 +19,7 @@ function HomePage(props: any) {
         <p>This has just turned into a stream of conciousness.</p>
         <p>Like, why have prices on eggs gone up so much lately? Crazy.</p>
         <p>Or the fact that a project I really liked, Blinks by Move38, is halting production.</p>
-        <p>That's a real bummer.</p>
+        <p>That&apos;s a real bummer.</p>
         <p>I am still putting off coding.</p>
       </div>
     </div>
